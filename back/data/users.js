91cf0e2f476791fc6240ed6d374f3f8433059ece
Aashutosh sharma <aashutoshsharma568@gmail.com>
2023-04-11 10:32:32 +0530

    first commit

---
+++ back/data/users.js
@@ -0,0 +1,21 @@
+
+const users = [
+  {
+    name: 'Admin User',
+    email: 'admin@example.com',
+    password: "123456",
+    isAdmin: true,
+  },
+  {
+    name: 'xyz',
+    email: 'xyz@example.com',
+    password: "123456",
+  },
+  {
+    name: 'abc',
+    email: 'abc@example.com',
+    password: "123456",
+  },
+]
+
+export default users
